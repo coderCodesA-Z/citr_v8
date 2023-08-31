c8c2e0f7e948908b222063c436663c2ae62b38fc
Sayan Dutta <rockistuart@gmail.com>
2023-08-31 21:54:33 +0530

    hooks: useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import SearchParams from "./SearchParams";
 
-import Pet from "./Pet";
-
-// vite does live code inclusion: tree shaking. So we don't need to worry about just importing the chunks
-// from the react libraries. Things will be handled by react
-
-// But it's always good to import stuffs that we actually need
-
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mix",
-    }),
-  ]);
-};
-
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const App = () => (
+  <div>
+    <h1>Adopt Me!</h1>
+    <SearchParams />
+  </div>
+);
 
 export default App;
---
+++ src/SearchParams.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+
+import Pet from "./Pet";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+// useEffect: does something out of the component lifecycle
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const breeds = ["Poodle"];
+
+  useEffect(() => {
+    requestPets();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`,
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
+
+  // in jsx, under form, inside the larger div
+  {
+    pets.map((pet) => (
+      <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+    ));
+  }
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option value="" disabled selected>
+              Select an Animal
+            </option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option value="" disabled selected>
+              Select a Breed
+            </option>
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
