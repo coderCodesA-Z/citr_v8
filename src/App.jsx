e3e9152385bb8b3021af45ddb81b7de6ea14cff4
Sayan Dutta <rockistuart@gmail.com>
2023-08-31 11:39:13 +0530

    starting to refactor everything to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import Pet from "./Pet";
+
 // vite does live code inclusion: tree shaking. So we don't need to worry about just importing the chunks
 // from the react libraries. Things will be handled by react
 
 // But it's always good to import stuffs that we actually need
 
-const Pet = (props) => {
-  // react supports one way data binding using props
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
@@ -38,3 +32,5 @@ const App = () => {
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
+
+export default App;
